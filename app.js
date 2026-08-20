f5e65443fc5964eebb32d2484bae639a25f30b10
guy33liba <81708094+guy33liba@users.noreply.github.com>
2026-08-20 15:55:47 +0300

    Add floating tools modal

--- app.js
+++ app.js
@@ -179,3 +179,12 @@ function prefersReducedMotion() {
 }
 
 init();
+
+(() => {
+  if (window.__FREE_TOOLS_WIDGET_LOADER__) return;
+  window.__FREE_TOOLS_WIDGET_LOADER__ = true;
+  const script = document.createElement('script');
+  script.src = 'https://appointments-schedule.netlify.app/tools-widget.js';
+  script.defer = true;
+  document.head.append(script);
+})();
